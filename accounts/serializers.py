b35d655644c2b2fbc75a26a929e288462af85fd4
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-28 12:08:00 +0300

    updated serializers with new imports and added a login class

---
+++ accounts/serializers.py
@@ -0,0 +1,14 @@
+from accounts.models import User
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
+
+class LoginSerializer(serializers.Serializer):
+	username = serializers.CharField()
+	password = serializers.CharField()
+
+	def validate(self, data):
+		user = authenticate(**data)
+		if user:
+			return user
+		raise serializers.ValidationError('Incorrect Credentials')
